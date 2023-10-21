0cb26788805f8499bb18e5628c1b9fc461a1e9df
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-21 09:00:06 -0300

    chore: add credo dependency

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule GithubApi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
