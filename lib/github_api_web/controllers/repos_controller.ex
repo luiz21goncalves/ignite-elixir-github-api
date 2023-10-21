4d2013cb9d40e3f798cc21ef68750590e8a3039a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-21 09:31:25 -0300

    feat: list user repos

--- lib/github_api_web/controllers/repos_controller.ex
+++ lib/github_api_web/controllers/repos_controller.ex
@@ -1,9 +1,31 @@
 defmodule GithubApiWeb.ReposController do
+  alias GithubApi.Github.Client
   use GithubApiWeb, :controller
 
   def show(conn, %{"username" => username}) do
-    conn
-    |> put_status(:ok)
-    |> json(%{username: username})
+    with {:ok, raw_repos} <- Client.get_user_repos(username),
+         repos <- format_repos(raw_repos) do
+      conn
+      |> put_status(:ok)
+      |> json(%{repos: repos})
+    end
+  end
+
+  defp format_repos(repos) do
+    Enum.map(repos, fn %{
+                         "id" => id,
+                         "name" => name,
+                         "description" => description,
+                         "html_url" => html_url,
+                         "stargazers_count" => stargazers_count
+                       } ->
+      %{
+        id: id,
+        name: name,
+        description: description,
+        html_url: html_url,
+        stargazers_count: stargazers_count
+      }
+    end)
   end
 end
