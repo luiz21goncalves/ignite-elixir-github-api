e1724c42ce3de01781cdca50c91efdab654305b3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-21 09:10:36 -0300

    chore: add tesla dependency

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule GithubApi.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:tesla, "~> 1.7"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
