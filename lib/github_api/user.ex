46ffb295d3bca441c81360af1a59e1e752f50abd
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-30 20:04:57 -0300

    chore: add user struct

---
+++ lib/github_api/user.ex
@@ -0,0 +1,47 @@
+defmodule GithubApi.User do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:name, :email, :password]
+
+  @mail_regex ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
+
+  @derive {Jason.Encoder, only: []}
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    validate(%__MODULE__{}, params)
+  end
+
+  defp validate(struct, params) do
+    "#{params} oi"
+
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 6)
+    |> validate_format(:email, @mail_regex)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    password_hash = Pbkdf2.hash_pwd_salt(password)
+
+    change(changeset, %{password_hash: password_hash})
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
